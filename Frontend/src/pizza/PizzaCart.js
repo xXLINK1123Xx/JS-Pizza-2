a51e6e588d8499b3833a8e963e984cd0918149c6
voidstorm <sasha.yaroshepta@gmail.com>
2016-06-16 18:26:32 +0300

    Revert "finished"
    This reverts commit 3279d652fe328792ba68f812f41bc9574833c06a.

--- Frontend/src/pizza/PizzaCart.js
+++ Frontend/src/pizza/PizzaCart.js
@@ -11,30 +11,19 @@ var PizzaSize = {
 
 //Змінна в якій зберігаються перелік піц в кошику
 var Cart = [];
-var cart_map = [];
-var count = 0;
 
 //HTML едемент куди будуть додаватися піци
-var $cart = $("#cartList");
+var $cart = $("#cart");
 
 function addToCart(pizza, size) {
+    //Додавання однієї піци в кошик покупок
 
-    var cart_id = cart_map[pizza.id];
-
-    if (cart_id && (cart_id[size] || cart_id[size] === 0)){
-            Cart[cart_id[size]].quantity += 1;
-            
-    } else {
-        if (!cart_map[pizza.id]){
-            cart_map[pizza.id] = [];
-        }
-        cart_map[pizza.id][size] = 
-        Cart.push({
-            pizza: pizza,
-            size: size,
-            quantity: 1
-        }) - 1;
-    }
+    //Приклад реалізації, можна робити будь-яким іншим способом
+    Cart.push({
+        pizza: pizza,
+        size: size,
+        quantity: 1
+    });
 
     //Оновити вміст кошика на сторінці
     updateCart();
@@ -44,12 +33,6 @@ function removeFromCart(cart_item) {
     //Видалити піцу з кошика
     //TODO: треба зробити
 
-    if (cart_item.cart_id){
-        Cart.splice(cart_item.cart_id, 1);
-        console.log("Cart length rem = "+Cart.length);
-            
-    }
-
     //Після видалення оновити відображення
     updateCart();
 }
@@ -58,24 +41,8 @@ function initialiseCart() {
     //Фукнція віпрацьвуватиме при завантаженні сторінки
     //Тут можна наприклад, зчитати вміст корзини який збережено в Local Storage то показати його
     //TODO: ...
-    var cart = localStorage.getItem("cart");
-    if (cart) 
-        Cart = JSON.parse(cart);
-
-    $("#clear").click(clearCart);
 
     updateCart();
-}
-
-function clearCart(){
-    Cart = [];
-    cart_map = [];
-
-    
-    updateCart();
-    count = 0;
-
-    $("#count").html(count);
 }
 
 function getPizzaInCart() {
@@ -91,59 +58,24 @@ function updateCart() {
     $cart.html("");
 
     //Онволення однієї піци
-    function showOnePizzaInCart(cart_item, id, arr) {
+    function showOnePizzaInCart(cart_item) {
         var html_code = Templates.PizzaCart_OneItem(cart_item);
-
-        cart_item.cart_id = id;
 
         var $node = $(html_code);
 
         $node.find(".plus").click(function(){
             //Збільшуємо кількість замовлених піц
             cart_item.quantity += 1;
-            count +=1;
+
             //Оновлюємо відображення
             updateCart();
         });
-
-        $node.find(".minus").click(function(){
-            
-            if (cart_item.quantity > 1){
-                cart_item.quantity -= 1;
-                count -=1;
-            }
-            
-            //Оновлюємо відображення
-            updateCart();
-        });
-
-
-        $node.find(".count-clear").click(function(){
-             
-            count -= cart_item.quantity;
-            console.log(count);
-            $("#count").html(count);
-             arr.splice(id, 1);
-
-            updateCart();
-        });
-        count = 0;
-        if(Cart.length != 0){
-            console.log("Cart length = "+Cart.length);
-            Cart.forEach(function(pizza){
-                count += pizza.quantity;
-            });
-        }
-        else count = 0;
-        console.log(count);
-        $("#count").html(count);
 
         $cart.append($node);
     }
 
     Cart.forEach(showOnePizzaInCart);
 
-    localStorage.setItem("cart", JSON.stringify(Cart));
 }
 
 exports.removeFromCart = removeFromCart;
